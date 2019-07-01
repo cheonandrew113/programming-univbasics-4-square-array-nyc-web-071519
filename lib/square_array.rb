e5fd1d933e456a46c0cb884eb457436c7d624d84
Andnrew Cheon <cheonandrew113@gmail.com>
2019-07-01 19:22:07 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,14 @@
 # does not call on each, collect, map, or inject'
 # should square the elements in an array'
 def square_array(array)
+  doubled_num = []
   
+  i = 0
+  
+  while i < numbers.length
+    old_num = numbers[i]
+    new_num = old_num * 2
+	doubled_num << new_num
+    
+    i += 1
 end

0643729e05a5e528a40592066ac78abca3f14295
Andnrew Cheon <cheonandrew113@gmail.com>
2019-07-01 19:21:21 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,5 @@
-
-
+# does not call on each, collect, map, or inject'
+# should square the elements in an array'
 def square_array(array)
-  # does not call on each, collect, map, or inject'
-  # should square the elements in an array'
-  
   
 end
